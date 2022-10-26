f4d28c0be60ae35dc08624e0a78785e1c1b28371
Zhao KK <kikakkz@hotmail.com>
2022-10-26 10:00:19 +0000

    Add get withdraw account

--- pkg/user/withdrawquery.go
+++ pkg/user/withdrawquery.go
@@ -2,10 +2,56 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	npool "github.com/NpoolPlatform/message/npool/account/gw/v1/user"
+
+	useraccmwcli "github.com/NpoolPlatform/account-middleware/pkg/client/user"
+	usermwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
+	coininfocli "github.com/NpoolPlatform/sphinx-coininfo/pkg/client"
 )
 
 func GetWithdrawAccount(ctx context.Context, id string) (*npool.Account, error) {
-	return nil, nil
+	info, err := useraccmwcli.GetAccount(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	if info == nil {
+		return nil, fmt.Errorf("invalid account")
+	}
+
+	u, err := usermwcli.GetUser(ctx, info.AppID, info.UserID)
+	if err != nil {
+		return nil, err
+	}
+	if u == nil {
+		return nil, fmt.Errorf("invalid user")
+	}
+
+	coin, err := coininfocli.GetCoinInfo(ctx, info.CoinTypeID)
+	if err != nil {
+		return nil, err
+	}
+	if coin == nil {
+		return nil, fmt.Errorf("invalid coin")
+	}
+
+	acc := &npool.Account{
+		ID:           info.ID,
+		AppID:        info.AppID,
+		UserID:       info.UserID,
+		CoinTypeID:   info.CoinTypeID,
+		CoinName:     coin.Name,
+		CoinUnit:     coin.Unit,
+		CoinEnv:      coin.ENV,
+		CoinLogo:     coin.Logo,
+		AccountID:    info.AccountID,
+		Address:      info.Address,
+		UsedFor:      info.UsedFor,
+		CreatedAt:    info.CreatedAt,
+		PhoneNO:      u.PhoneNO,
+		EmailAddress: u.EmailAddress,
+	}
+
+	return acc, nil
 }
