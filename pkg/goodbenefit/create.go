fe83cad9c36261e2017aed7f9be04db777091e89
jakys <jakysing@163.com>
2024-06-05 02:12:49 +0000

    Fix http://47.116.78.131:10083/issues/3578

--- pkg/goodbenefit/create.go
+++ pkg/goodbenefit/create.go
@@ -7,10 +7,12 @@ import (
 	addresscheck "github.com/NpoolPlatform/account-gateway/pkg/addresscheck"
 	gbmwcli "github.com/NpoolPlatform/account-middleware/pkg/client/goodbenefit"
 	coinmwcli "github.com/NpoolPlatform/chain-middleware/pkg/client/coin"
+	goodmwcli "github.com/NpoolPlatform/good-middleware/pkg/client/good"
 	goodcoinmwcli "github.com/NpoolPlatform/good-middleware/pkg/client/good/coin"
 	npool "github.com/NpoolPlatform/message/npool/account/gw/v1/goodbenefit"
 	gbmwpb "github.com/NpoolPlatform/message/npool/account/mw/v1/goodbenefit"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	goodmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/good"
 	goodcoinmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/good/coin"
 	sphinxproxypb "github.com/NpoolPlatform/message/npool/sphinxproxy"
 	sphinxproxycli "github.com/NpoolPlatform/sphinx-proxy/pkg/client"
@@ -24,6 +26,19 @@ type createHandler struct {
 	checkAddressBalance bool
 	backup              bool
 	address             *string
+}
+
+func (h *createHandler) getGood(ctx context.Context) error {
+	exist, err := goodmwcli.ExistGoodConds(ctx, &goodmwpb.Conds{
+		EntID: &basetypes.StringVal{Op: cruder.EQ, Value: *h.GoodID},
+	})
+	if err != nil {
+		return err
+	}
+	if !exist {
+		return fmt.Errorf("invalid good")
+	}
+	return nil
 }
 
 func (h *createHandler) getGoodCoin(ctx context.Context) error {
@@ -129,6 +144,9 @@ func (h *Handler) CreateAccount(ctx context.Context) (*npool.Account, error) {
 		Handler: h,
 	}
 
+	if err := handler.getGood(ctx); err != nil {
+		return nil, err
+	}
 	if err := handler.getGoodCoin(ctx); err != nil {
 		return nil, err
 	}
